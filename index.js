b0aa97803e87376d86004bd3b0ff993f67417a7a
Netson <netsoncavina@gmail.com>
2022-08-19 13:06:19 -0300

    correção

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
-app.use("user", userRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
