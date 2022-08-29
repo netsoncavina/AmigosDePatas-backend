74933f568d1b9514aaa4ac93e0c929144da1ce82
Netson <netsoncavina@gmail.com>
2022-08-29 12:01:23 -0300

    adicionado resposta para verificar servidor rodando

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Servidor rodando");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
