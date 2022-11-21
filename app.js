a52ebbc51e59f7461073345e1c6ee19d6931f46a
Netson <netsoncavina@gmail.com>
2022-11-21 16:16:42 -0300

    refatoração para o uso do supertest

--- app.js
+++ app.js
@@ -23,15 +23,4 @@ app.get("/", (req, res) => {
   res.send("Servidor rodando");
 });
 
-const PORT = process.env.PORT || 5000;
-const DB_CONNECTION =
-  "mongodb+srv://netson:cwxy4Keg4ODkTTRe@cluster0.e3ugm.mongodb.net/projetointegrador?retryWrites=true&w=majority";
-mongoose
-  .connect(DB_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-  )
-  .catch((error) => console.log(error.message, process.env.JWT_SECRET));
+export default app;
