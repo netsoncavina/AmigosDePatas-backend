158cf05d939f00ae9d27075e7d5c140c5ee260bd
Netson <netsoncavina@gmail.com>
2023-01-11 20:13:43 -0300

    adicionado rota de atualização de usuario

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { signin, signup, getUsers } from "../controllers/userController.js";
+import {
+  signin,
+  signup,
+  getUsers,
+  updateUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.patch("/:id", updateUser);
 router.post("/signin", signin);
 router.post("/signup", signup);
 
