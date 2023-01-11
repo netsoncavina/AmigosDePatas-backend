edac94ffffeda465a9cdf99de58563397f89f1ef
Netson <netsoncavina@gmail.com>
2023-01-11 19:36:30 -0300

    adicionada rota getUsers

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signin, signup } from "../controllers/userController.js";
+import { signin, signup, getUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 
+router.get("/", getUsers);
 router.post("/signin", signin);
 router.post("/signup", signup);
 
